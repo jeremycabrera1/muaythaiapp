0431cdadaa8bc37f95707bab4f5ad5caffb1634b
jeremycabrera1 <jeremycabrera1000@gmail.com>
2025-09-16 13:37:39 -0400

    almost ready for my portfolio

--- schedule/models.py
+++ schedule/models.py
@@ -18,7 +18,7 @@ class Registration(models.Model):
     name = models.CharField(max_length=100)
     last_name = models.CharField(max_length=100)
     email = models.EmailField(unique=True)
-    phone_number = PhoneNumberField(
-        region='US', null=False, blank=False, unique=True)
+    phone_number = PhoneNumberField(region='US', null=False, blank=False, unique=True)
 
     def __str__(self):
+        return f'{self.name} {self.last_name}'
