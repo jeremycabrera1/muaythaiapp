bf881f0edeec4217b0e73608dd3cf8b2d49feb34
jeremycabrera1 <jeremycabrera1000@gmail.com>
2025-10-13 11:23:57 -0400

    reviews

--- schedule/models.py
+++ schedule/models.py
@@ -1,11 +1,12 @@
 from django.db import models
+from django.core.validators import MaxValueValidator
 from phonenumber_field.modelfields import PhoneNumberField
 
 # Create your models here.
 
 
 class MuayClass1(models.Model):
-    title = models.CharField(max_length=200, default='Untitled')
+    title = models.CharField(max_length=100, default='Untitled')
     coach = models.CharField(max_length=100)
     start_time = models.TimeField()
     end_time = models.TimeField()
@@ -22,3 +23,13 @@ class Registration(models.Model):
 
     def __str__(self):
         return f'{self.name} {self.last_name}'
+    
+
+class Reviews(models.Model):
+    user = models.ForeignKey(Registration, on_delete=models.CASCADE, related_name='reviews')
+    rating = models.PositiveSmallIntegerField(default=1, validators=[MaxValueValidator(5)])
+    review = models.TextField()
+    date = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f'{self.user.name} {self.user.last_name}'
